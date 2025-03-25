1e9c9ce191556b5296031d2ba46753426f576401
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2025-03-24 20:28:38 -0500

    ADD AdminService

---
+++ src/main/java/com/promo_viajes/api/domain/service/AdminService.java
@@ -0,0 +1,49 @@
+package com.promo_viajes.api.domain.service;
+
+import com.promo_viajes.api.domain.dto.AdminDTO;
+import com.promo_viajes.api.domain.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class AdminService {    
+    
+    @Autowired
+    private AdminRepository adminRepository;
+    
+     // Consultar todos los registros
+    public Iterable<AdminDTO> findAll(){
+        return adminRepository.findAll();
+    }
+
+    // Consultar por ID
+    public Optional<AdminDTO> findById(Long id) {
+        return adminRepository.findById(id);
+    }
+
+    // Guardar un registro
+    public AdminDTO save(AdminDTO adminDTO) {
+        return adminRepository.save(adminDTO);
+    }
+
+    // Actualizar un registro
+    public AdminDTO update(AdminDTO adminDTO) {
+        return adminRepository.update(adminDTO);
+    }
+
+    // Eliminar un registro
+    public void delete(Long id) {
+        adminRepository.delete(id);
+    }
+
+    // Validar si existe por ID
+    public boolean existsById(Long id) {
+        return adminRepository.existsById(id);
+    }
+
+    // Contar todos los registros
+    public long count() {
+        return adminRepository.count();
+    }
+}
