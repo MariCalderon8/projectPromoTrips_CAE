14829c73574d8e6d186e1a4be4360a53b9ec4a80
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2025-03-25 20:03:52 -0500

    ADD TripRepository interface

---
+++ src/main/java/com/promo_viajes/api/domain/repository/TripRepository.java
@@ -0,0 +1,35 @@
+package com.promo_viajes.api.domain.repository;
+
+
+import com.promo_viajes.api.domain.dto.TripDTO;
+
+import java.util.Optional;
+
+public interface TripRepository {
+    // Consultar todos los registros
+    Iterable<TripDTO> findAll();
+
+    // Consultar por ID
+    Optional<TripDTO> findById(Long id);
+
+    // Guardar un registro
+    TripDTO save(TripDTO tripDTO);
+
+    // Actualizar un registro
+    TripDTO update(TripDTO tripDTO);
+
+    // Eliminar un registro
+    boolean delete(Long id);
+
+    // Validar si existe por ID
+    boolean existsById(Long id);
+
+    // Consultar todos los viajes a un precio específico o menor
+    Iterable<TripDTO> findTripsByPriceOrLower(float price);
+
+    // Consultar viajes en un fecha especifica
+    Iterable<TripDTO> findTripsByDate(float price);
+
+    // Contar todos los registros
+    long count();
+}
