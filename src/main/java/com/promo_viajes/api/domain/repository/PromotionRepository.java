727bde2a77573363f13fd4e461222a9458ab2541
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2025-03-25 21:41:24 -0500

    ADD PromotionRepository interface

---
+++ src/main/java/com/promo_viajes/api/domain/repository/PromotionRepository.java
@@ -0,0 +1,38 @@
+package com.promo_viajes.api.domain.repository;
+
+
+import com.promo_viajes.api.domain.dto.PromotionDTO;
+
+import java.util.Optional;
+
+public interface PromotionRepository {
+    // Consultar todos los registros
+    Iterable<PromotionDTO> findAll();
+
+    // Consultar por ID
+    Optional<PromotionDTO> findById(Long id);
+
+    // Guardar un registro
+    PromotionDTO save(PromotionDTO promotionDTO);
+
+    // Actualizar un registro
+    PromotionDTO update(PromotionDTO promotionDTO);
+
+    // Eliminar un registro
+    boolean delete(Long id);
+
+    // Validar si existe por ID
+    boolean existsById(Long id);
+
+    // Calcular el valor del viaje con descuento
+    float calcuteTripUpdatedPrice(Long tripId);
+
+    // Consultar todas las promociones activas
+    Iterable<PromotionDTO> findAllActivesPromotions();
+
+    // Consultar todas las promociones de un viajes
+    Iterable<PromotionDTO> findAllPromotionsByTrip(Long tripId);
+
+    // Contar todos los registros
+    long count();
+}
